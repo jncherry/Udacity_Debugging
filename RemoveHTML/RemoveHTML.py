ee3e9864ced00ef6e2c928408208ac8d42a08c70
jncherry <jason.cherry@ymail.com>
2015-04-11 15:19:24 -0700

    Initial issue resolved, html is now correctly stripped when quotes are present in all cases.

--- RemoveHTML/RemoveHTML.py
+++ RemoveHTML/RemoveHTML.py
@@ -4,15 +4,20 @@ def remove_html_markup(s):
     out = ""
 
     for c in s:
+
         if c == '<' and not quote:
             tag = True
         elif c == '>' and not quote:
             tag = False
-        elif c == '"' or c == '"' and tag:
+        elif (c == '"' or c == "'") and tag:
             quote = not quote
         elif not tag:
             out = out + c
                 
     return out
 
-print remove_html_markup("'<b>foo</b>'")
+print remove_html_markup('"foo"')
+print remove_html_markup("'foo'")
+print remove_html_markup("<b>foo</b>")
+print remove_html_markup('"<b>foo</b>"')
+print remove_html_markup("foo")
